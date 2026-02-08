5087b3891c0694687f062ee58b8214fdedc6a15f
Alena <alporokh@gmail.com>
2026-02-08 12:15:30 +0200

    update

--- js/task-3.js
+++ js/task-3.js
@@ -3,8 +3,6 @@
   return normalizedMessage.includes("spam") || normalizedMessage.includes("sale");
 }
     
-
-
 console.log(checkForSpam("Latest technology news")); // false
 console.log(checkForSpam("JavaScript weekly newsletter")); // false
 console.log(checkForSpam("Get best sale offers now!")); // true
