4ee1d5d5a756cfef3c5aecaab69039703b98ac39
Alena <alporokh@gmail.com>
2026-02-08 11:55:17 +0200

    update

--- js/task-4.js
+++ js/task-4.js
@@ -1,12 +1,26 @@
-getShippingCost(country) {
-      switch (country) {
-         case "China": return 100;
-         case "Chile": return 250;
-         case "Australia": return 170;
-         case "Jamaica": return 120;
-         default: return "Sorry, there is no delivery to your country";
-      }
+function getShippingCost(country) {
+  let cost;
+
+  switch (country) {
+    case "China":
+      cost = 100;
+      break;
+    case "Chile":
+      cost = 250;
+      break;
+    case "Australia":
+      cost = 170;
+      break;
+    case "Jamaica":
+      cost = 120;
+      break;
+    default:
+      return "Sorry, there is no delivery to your country";
+  }
+
+  return `Shipping to ${country} will cost ${cost} credits`;
 }
+
 
 
 console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
